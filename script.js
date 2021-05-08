baaec7700851bb7538b4ce07a06f36b3a4e3390a
Aayush Shrestha <aayush.shrestha@ontariotechu.net>
2021-05-08 17:08:47 -0400

    about panel slides in and out of dms

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+function openAbout() {
+    document.getElementById("sidePanel").style.width = "250px";
+}
+
+function closeAbout() {
+    document.getElementById("sidePanel").style.width = "0px";
+}
